af333928c8f016bf0dbde3193511baadcd6e971d
luna.le <luna.le@i-on.net>
2023-07-04 14:02:56 +0700

    Change product data

--- src/App.js
+++ src/App.js
@@ -21,16 +21,13 @@ export const App = () => {
   const [prod, setProd] = useState()
   
   const fetch = async () => {
-    const token = '5mL3COW_uUeaDjT5evIZUPVU1EAMd8jKJNiTu0LqwlGbK6xt6nL4isYJP4UCoU8dAVzOsSK8i0Puoid0gfxryQ'
     const config = {
-      withCredentials: true,
-      baseURL: 'http://localhost:8080/svc/mfeProductApi',
-      headers: {
-        'Authorization': 'Bearer' + token,
-        'Access-Control-Allow-Origin': '*',
+      auth: {
+        username: 'kminchelle',
+        password: '0lelplR'
       }
     }
-    await axios.get('/list?_siteId=brand', config).then(res => setProd(res.data)).catch(console.log)
+    await axios.get('https://dummyjson.com/products', config).then(res => setProd(res.data)).catch(console.log)
     return
   }
   useEffect(() => {
@@ -41,15 +38,15 @@ export const App = () => {
     <>
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Total: {prod?.totalCount}</h2>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">Total: {prod?.total}</h2>
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            {prod?.items.map((product) => (
+            {prod?.products.map((product) => (
                 <div key={product.id} className="group relative">
                 <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                   <img
                     src={product.thumbnail}
                     alt="Products"
-                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full border border-gray-100"
                   />
                 </div>
                 <div className="mt-4 flex justify-between">
@@ -61,9 +58,9 @@ export const App = () => {
                       </a>
                     </h3>
                     <p className="mt-1 mb-1 text-sm text-gray-500">{product.description}</p>
-                    <p className="text-sm font-medium text-gray-900">{product.brand}</p>
+                    <p className="text-sm font-medium text-gray-900 bg-gray-200 p-1 w-fit">{product.brand}</p>
                   </div>
-                  <p className="text-sm font-medium text-gray-900">${product.price}</p>
+                  <p className="text-sm font-medium text-red-600">${product.price}</p>
                 </div>
               </div>
             ))}
